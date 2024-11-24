5c1c908f9f48a73dba40bfb437d296fc6dcc6fdf
Patrick Kodal <6306551+ladokp@users.noreply.github.com>
2024-11-24 21:18:52 +0100

    added fractal plant snippet

---
+++ python/fractal_plant.py
@@ -0,0 +1,42 @@
+import turtle
+
+def l_system(axiom, rules, iterations):
+    """Generiert ein L-System basierend auf Regeln und Iterationen."""
+    for _ in range(iterations):
+        axiom = ''.join(rules.get(ch, ch) for ch in axiom)
+    return axiom
+
+def draw_l_system(axiom, angle, length):
+    """Zeichnet das L-System mit Turtle Graphics."""
+    stack = []
+    for command in axiom:
+        if command == 'F':
+            turtle.forward(length)
+        elif command == '+':
+            turtle.right(angle)
+        elif command == '-':
+            turtle.left(angle)
+        elif command == '[':
+            stack.append((turtle.pos(), turtle.heading()))
+        elif command == ']':
+            pos, heading = stack.pop()
+            turtle.penup()
+            turtle.setpos(pos)
+            turtle.setheading(heading)
+            turtle.pendown()
+
+# Regeln für eine einfache Fraktalpflanze
+rules = {
+    'F': 'FF+[+F-F-F]-[-F+F+F]'
+}
+axiom = "F"
+iterations = 4
+angle = 25
+length = 10
+
+# L-System generieren und zeichnen
+turtle.speed(0)
+turtle.left(90)  # Startwinkel nach oben
+commands = l_system(axiom, rules, iterations)
+draw_l_system(commands, angle, length)
+turtle.done()
